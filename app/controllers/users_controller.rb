94fdd126699edb89858e20324141e07b613a96f1
Minh <mhnguyen289@gmail.com>
2016-07-18 09:40:18 -0700

    updated user profile with edit/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,36 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update]
 
-	def index
-	end
+  before_action :set_user, only: [:edit, :update]
+  
+  def edit
+  end
 
-	def edit
-	end
-
-	def show 
-		@users = User.all
-	end
-
-	def update
-		if @user.update(user_params)
+  def update
+    if @user.update(user_params)
        flash[:notice] = "User profile successfully updated"
        redirect_to @user
      else
        flash[:notice] = "Oops something went wrong. Please try again."
        render :edit 
      end
-	end
+  end
 
-	private
+  def index
+    @users = User.all
+  end
 
-	def user_params
-		    params.require(:user).permit(:username, :age, :hometown, :info, :email, :password, :password_confirmation) 
-	end
+  def show 
+    @user = User.find(params[:id])
+  end 
 
-	def set_user
-		@user = current_user
-	end
+ private
 
+  def user_params
+    params.require(:user).permit(:username, :age, :hometown, :info, :email, :password, :password_confirmation) 
+  end
+
+  def set_user
+    @user = current_user
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/index'
-  resources :notes
+  resources :notes, only: [:index, :new, :edit, :show]
+  resources :users, only: [:index, :new, :edit, :show, :update]
 
   authenticated :user do
   	root 'notes#index', as: "authenticated_root"

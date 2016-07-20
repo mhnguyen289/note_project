fe2071a902f94b2fc5f2863d99f5a66c66a78013
Minh <mhnguyen289@gmail.com>
2016-07-20 14:20:09 -0700

    added crud and route to notebook

---
+++ app/controllers/notebooks_controller.rb
@@ -0,0 +1,45 @@
+class NotebooksController < ApplicationController
+
+	before_action :find_notbook, only [:show, :edit, :update, :destroy]
+
+	def index
+		@notes = Note.where(user_id: current_user)
+	end
+
+	def show
+	end
+
+	def new
+		@note = current_user.notes.build
+	end
+
+	def create
+		@note = current_user.notes.build(notebook_params)
+
+		if @notebook.save
+			redirect_to @notebook
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def notebook_params
+		params.require(:destination)
+
+	def find_notbook
+		@notebook = Notebook.find(params[:id])
+	end	
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :notes
   resources :users
+  resources :notes
+  resources :notebooks
 
   authenticated :user do
   	root 'notes#index', as: "authenticated_root"

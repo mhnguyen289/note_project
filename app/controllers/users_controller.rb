002f8f52185476a9d7069e45f80846b93d0c8b2d
Minh <mhnguyen289@gmail.com>
2016-07-27 14:43:43 -0700

    fixed log out bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
   def show 
     @user = User.find(params[:id])
-    @notebook = Notebook.all
+    @notebooks = Notebook.all
   end 
 
  private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+   devise_scope :user do
+  	 get '/users/sign_out' => 'devise/sessions#destroy' 
+end
 
   get 'welcome/index'
-  resources :users
+  resources :users 
+
   resources :notes
   resources :notebooks
 
@@ -12,5 +16,5 @@ Rails.application.routes.draw do
 
 
   root 'welcome#index'
-
 end
+
